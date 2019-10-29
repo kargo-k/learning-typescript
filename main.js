195e72e77eae26449e844e314bb2d7e7b9c15eb8
kargo <karenrgo@gmail.com>
2019-10-28 21:24:31 -0700

    typescripts types

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict";
 exports.__esModule = true;
+// tsc main.ts --watch --> watches file changes and compiles with each change
+// tsc main.ts --> compiles into js file when run
 // Typescript types: boolean, number, string
 var name = 'Karen';
 var num = 3.14;
@@ -10,5 +12,66 @@ console.log(greeting, flag);
 var num2 = undefined;
 console.log('num2 should be undefined: ', num2);
 num2 = 4;
-console.log(num2);
+console.log('redefining num2 to a number: ', num2);
 var flag2 = null;
+flag2 = true;
+// Unions -- able to pass multiple types of a variable
+var union = 3;
+console.log("Union: " + union);
+union = null;
+console.log("Union: " + union);
+// Array declaration
+var list = [1, 2, 3];
+var list2 = [3, 2, 1];
+// Tuple - express an array with a fixed number of elements with known types, but does not need to be the same type.
+var x;
+x = ['hello', 10];
+// Enum - "more friendly names to sets of numeric values".  Makes a new datatype that is 0-indexed
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+var c = Color.Green;
+console.log(Color);
+console.log(c);
+// access the value based on the index
+console.log(Color[1]);
+// can also override the index
+var Colors;
+(function (Colors) {
+    Colors[Colors["Red"] = 1] = "Red";
+    Colors[Colors["Green"] = 2] = "Green";
+    Colors[Colors["Blue"] = 4] = "Blue";
+})(Colors || (Colors = {}));
+console.log(Colors[1]);
+// Any Type -- can create a variable that can change types
+var notSure = 4;
+notSure = "I can be a string";
+notSure = false; // change it to a boolean
+var anyList = [195, true, "string"];
+console.log(anyList[1]);
+// Void type -- absence of having any type.  Usually see this as the return type of functions that do not have a return value
+function warnUser() {
+    console.log('warning warning warning');
+}
+// Never type -- typically used as the returnt ype of a function expression that always throws an exception or never returns
+function infiniteLoop() {
+    while (true) {
+    }
+}
+function error(message) {
+    throw new Error(message);
+}
+create({ property: 0 }); // OK
+create(null); // OK
+// create(42) // Error
+// create('string') // Error
+// Type Assertions -- a way to override the compiler checks
+// "angle bracket syntax"
+var someVal = 'this is a string';
+var strLen = someVal.length;
+// the "as syntax"
+var someValue = 'this is a string';
+var strLength = someValue.length;
--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 export { }
+// tsc main.ts --watch --> watches file changes and compiles with each change
+// tsc main.ts --> compiles into js file when run
+
 
 // Typescript types: boolean, number, string
 let name: string = 'Karen'
@@ -17,4 +20,64 @@ console.log('redefining num2 to a number: ', num2)
 let flag2: boolean = null;
 flag2 = true;
 
-// Example of a Tuple
+// Unions -- able to pass multiple types of a variable
+let union: number | null | undefined = 3;
+console.log(`Union: ${union}`)
+union = null
+console.log(`Union: ${union}`)
+
+// Array declaration
+let list: number[] = [1, 2, 3]
+let list2: Array<number> = [3, 2, 1]
+
+// Tuple - express an array with a fixed number of elements with known types, but does not need to be the same type.
+let x: [string, number];
+x = ['hello', 10]
+
+// Enum - "more friendly names to sets of numeric values".  Makes a new datatype that is 0-indexed
+enum Color { Red, Green, Blue }
+let c: Color = Color.Green
+console.log(Color)
+console.log(c)
+// access the value based on the index
+console.log(Color[1])
+// can also override the index
+enum Colors { Red = 1, Green = 2, Blue = 4 }
+console.log(Colors[1])
+
+// Any Type -- can create a variable that can change types
+let notSure: any = 4;
+notSure = "I can be a string"
+notSure = false // change it to a boolean
+let anyList: any[] = [195, true, "string"]
+console.log(anyList[1])
+
+// Void type -- absence of having any type.  Usually see this as the return type of functions that do not have a return value
+function warnUser(): void {
+  console.log('warning warning warning')
+}
+
+// Never type -- typically used as the returnt ype of a function expression that always throws an exception or never returns
+function infiniteLoop(): number {
+  while (true) {
+  }
+}
+
+function error(message: string): never {
+  throw new Error(message);
+}
+
+// Object - a type that represents the non-primitive type ... anything not a number, string, boolean, symbol, null, or undefined.
+declare function create(o: object | null): void;
+create({ property: 0 }) // OK
+create(null) // OK
+// create(42) // Error
+// create('string') // Error
+
+// Type Assertions -- a way to override the compiler checks
+// "angle bracket syntax"
+let someVal: any = 'this is a string'
+let strLen: number = (<string>someVal).length
+// the "as syntax"
+let someValue: any = 'this is a string'
+let strLength: number = (someValue as string).length
