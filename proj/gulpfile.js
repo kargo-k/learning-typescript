c6bbe51a82dbbdf85fe5d396b7a6c481fab99850
kargo <karenrgo@gmail.com>
2019-10-28 21:59:01 -0700

    added watchify and fancy log. refactored gulpfile to wrap browserify instance in watcify so that bundle will run when a TS file changes

--- proj/gulpfile.js
+++ proj/gulpfile.js
@@ -2,25 +2,32 @@ var gulp = require('gulp')
 var browserify = require('browserify')
 var source = require('vinyl-source-stream')
 var tsify = require('tsify')
+var fancy_log = require('fancy-log')
 var paths = {
   pages: ['src/*.html']
 }
+
+var watchedBrowserify = watchify(browserify({
+  basedir: '.',
+  debug: true,
+  entries: ['src/main.ts'],
+  cache: {},
+  packageCache: {}
+}).plugin(tsify))
 
 gulp.task('copy-html', function () {
   return gulp.src(paths.pages)
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('default', gulp.series(gulp.parallel('copy-html'), function () {
-  return browserify({
-    basedir: '.',
-    debug: true,
-    entries: ['src/main.ts'],
-    cache: {},
-    packageCache: {}
-  })
-    .plugin(tsify)
+function bundle() {
+  return watchedBrowserify
     .bundle()
+    .on('error', fancy_log)
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('dist'))
-}))
+}
+
+gulp.task('default', gulp.series(gulp.parallel('copy-html'), bundle));
+watchedBrowserify.on('update', bundle)
+watchedBrowserify.on('log', fancy_log)
